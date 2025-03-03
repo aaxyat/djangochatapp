50bf73232668e6d810fbd7aceeeaa7ab80a0aa9e
Ayush Bhattarai <git@ayushb.com>
2025-03-03 06:00:47 +0000

    feat: Add Category, Server, and Channel models with admin registration

--- backend/server/admin.py
+++ backend/server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from server.models import Category, Channel, Server
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Server)
+admin.site.register(Channel)
