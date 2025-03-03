77996837d21c272b7af3858c2d9e7b7ba0264077
Ayush Bhattarai <git@ayushb.com>
2025-03-03 05:36:22 +0000

    feat: Add account and server apps with initial configuration and models

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "account.apps.AccountConfig",
+    "server.apps.ServerConfig",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
